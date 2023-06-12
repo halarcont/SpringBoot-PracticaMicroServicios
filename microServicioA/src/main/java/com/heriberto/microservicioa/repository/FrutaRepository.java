1cd4abb62b5bc76ba739568b66769495556ade03
halarcont <herito9602@gmail.com>
2023-06-12 12:03:13 -0400

    Agregando un test

--- microServicioA/src/main/java/com/heriberto/microservicioa/repository/FrutaRepository.java
+++ microServicioA/src/main/java/com/heriberto/microservicioa/repository/FrutaRepository.java
@@ -1,9 +1,10 @@
 package com.heriberto.microservicioa.repository;
 
 import com.heriberto.microservicioa.entity.Fruta;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 
-public interface FrutaRepository extends CrudRepository<Fruta, Long> {
+public interface FrutaRepository extends JpaRepository<Fruta, Long> {
 
 }
+
---
+++ microServicioA/src/test/java/com/heriberto/microservicioa/test/FrutaRepositoryMockTest.java
@@ -0,0 +1,39 @@
+package com.heriberto.microservicioa.test;
+
+import com.heriberto.microservicioa.entity.Fruta;
+import com.heriberto.microservicioa.repository.FrutaRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+@DataJpaTest
+public class FrutaRepositoryMockTest {
+
+    @Autowired
+    private FrutaRepository frutaRepository;
+
+    @Test
+    public void findById() {
+        Fruta fruta = Fruta.builder()
+                .nombre("melon")
+                .precio(Double.parseDouble("8.98"))
+                .caducaEn(new Date())
+                .build();
+        frutaRepository.save(fruta);
+
+        Optional<Fruta> found = frutaRepository.findById(fruta.getId());
+
+        Assertions.assertAll(
+                () -> Assertions.assertTrue(found.isPresent()),
+                () -> Assertions.assertEquals(fruta.getId(), found.get().getId()),
+                () -> Assertions.assertEquals(fruta.getNombre(), found.get().getNombre()),
+                () -> Assertions.assertEquals(fruta.getPrecio(), found.get().getPrecio()),
+                () -> Assertions.assertEquals(fruta.getCaducaEn(), found.get().getCaducaEn())
+        );
+    }
+}
